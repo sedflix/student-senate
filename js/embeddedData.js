175fba293a1ec6365ad9469c6ec7ad122531b278
geekSiddharth <siddharth16268@iiitd.ac.in>
2017-06-20 21:06:55 +0530

    slack page added

--- js/embeddedData.js
+++ js/embeddedData.js
@@ -8,6 +8,8 @@ function loadDoc(name, two) {
         if (this.readyState == 4 && this.status == 200) {
             links[two] = this.response;
         }
+        else {
+        }
     };
     xhttp.open("GET", name.toString(), true);
     return xhttp.send();
@@ -25,7 +27,7 @@ var links = {
     "Submit Solution": '<iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrOXBdRSgSMU6U6D?backgroundColor=teal" frameborder="0" onmousewheel="" width="100%" height="100%" style="background: transparent; border: 1px solid #ccc;"></iframe>',
     "Software Projects": 'Under development',
     "Events": 'Under development',
-    "Join our Slack Channel": 'Under development',
+    "Join us on Slack": loadDoc('slack.html', "Join us on Slack"),
     "Mail Us": 'Under development',
     "Quick Link": '<iframe class="airtable-embed" src="https://airtable.com/embed/shrTXxDCIufvPbitu?backgroundColor=teal&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="100%" style="background: transparent; border: 1px solid #ccc;"></iframe>',
     "Submit a Quick Link": '<iframe class="airtable-embed" src="https://airtable.com/embed/shrTIAgqYnSCDpDFd?backgroundColor=teal" frameborder="0" onmousewheel="" width="100%" height="100%" style="background: transparent; border: 1px solid #ccc;"></iframe>',
